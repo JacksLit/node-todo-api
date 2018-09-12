3015a9c1a47c9bde5d255ab6445b8f50698a4261
Josh Zirena <jz1551@gmail.com>
2018-09-12 23:44:51 +0200

    finished server.test.js

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,8 +1,8 @@
 const expect = require('expect');
 const request = require('supertest');
 
-const {app} = require('./../server');
-const {Todo} = require('./../models/todo');
+const { app } = require('./../server');
+const { Todo } = require('./../models/todo');
 
 beforeEach((done) => {
   Todo.remove({}).then(() => done());
@@ -14,7 +14,7 @@ describe('POST /todos', () => {
 
     request(app)
       .post('/todos')
-      .send({text})
+      .send({ text })
       .expect(200)
       .expect((res) => {
         expect(res.body.text).toBe(text);
@@ -29,9 +29,23 @@ describe('POST /todos', () => {
           expect(todos[0].text).toBe(text);
           done();
         }).catch((e) => done(e));
+      });
+  });
 
+  it('should not create todo with invalid body data', (done) => {
+    request(app)
+      .post('/todos')
+      .send({ })
+      .expect(400)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
 
-
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
       });
   });
 });
