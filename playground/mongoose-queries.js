945a0cfc07d35910f117ff5e64e5ddebfaf8c8f3
Josh Zirena <jz1551@gmail.com>
2018-09-15 12:32:23 +0200

    Added queries to playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,53 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {Users} = require('./../server/models/users');
+
+let id = '5b866311fdc23032acb00085';
+
+if(!ObjectID.isValid(id)) {
+    console.log('ID is not valid!');
+} else {
+    Users.findById(id).then((user) => {
+        if (!user) {
+            return console.log('User not found');
+        }
+        console.log('User by id', user);
+    }).catch((e) => console.log('error.'));
+}
+
+
+
+// require user model
+// user.findById
+// check if there's a user
+// if user print to the screen
+// handle any errors 
+
+
+// let id = '5b9ccede567ffd61cdbf7f3911';
+
+// if (!ObjectID.isValid(id)) {
+//     console.log('ID is not valid');
+// }
+
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log('Todos', todos);
+// });
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//     if (!todo) {
+//         return console.log('id not found');
+//     }
+//     console.log('Todo By Id', todo);
+// }).catch((e) => console.log(e));
+
