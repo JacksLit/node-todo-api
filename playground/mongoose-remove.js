dd6a44fb11f17240f75db2c208a98f779220d3ea
Josh Zirena <jz1551@gmail.com>
2018-09-23 01:36:53 +0200

    test

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,17 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {Users} = require('./../server/models/users');
+
+// Todo.remove({}).then((result) => {
+//     console.log(result);
+// });
+
+// Todo.findOneAndRemove()
+// Todo.findByIdAndRemove()
+
+Todo.findByIdAndDelete('5ba413b84ddb4935503cb418').then((todo) => {
+    console.log(todo);
+});
+
